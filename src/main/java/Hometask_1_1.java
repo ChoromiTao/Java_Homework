48a33bec3ae2394176075b26677896b913713283
Choromi Tao <princessailish94@mail.ru>
2023-06-09 19:15:18 +0900

    Arrays are empty yet(((

--- src/main/java/Hometask_1_1.java
+++ src/main/java/Hometask_1_1.java
@@ -1,38 +1,43 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class Hometask_1_1 {
     public static void main(String[] args){
         int i = new Random().nextInt(2000);
-        int limit_min_short = Short.MIN_VALUE;
-        int limit_max_short = Short.MAX_VALUE;
-        int[] array_max_value = new int[limit_max_short];
-        int[] array_min_value = new int[Math.abs(limit_min_short)];
         System.out.println("случайное сгенерированное число: " + i);
         System.out.println("номер старшего значащего бита этого выпавшего числа: " + older_byte(i));
-        arr_max_value(array_max_value, i, older_byte(i));
-        arr_min_value(array_min_value, i, older_byte(i));
+        int bit_length = older_byte(i);
+        System.out.println("Значения, которые кратные n числа в диапазоне от i до Short.MAX_VALUE: ");
+        int[] array_max_value = arr_max_value(i, bit_length);
+        System.out.println(Arrays.toString(array_max_value));
+//        System.out.println("Значения, которые все некратные n числа в диапазоне от Short.MIN_VALUE до i: ");
+//        int[] array_min_value = arr_min_value(i, bit_length);
+//        System.out.println(Arrays.toString(array_min_value));
     }
     public static int older_byte(int i){
         String a = Integer.toBinaryString(i);
-        int new_length = a.length();
-        return new_length;
+        return a.length();
     }
-    public static void arr_max_value(int[] array_max_value, int i, int new_length){
-        System.out.println("Значения, которые кратные n числа в диапазоне от i до Short.MAX_VALUE: ");
-        for (int j = i, k = 0; j < (Short.MAX_VALUE) & new_length %j == 0; j++) {
-            array_max_value[k] = j;
-            k ++;
-            System.out.println(array_max_value[k]);
+    public static int[] arr_max_value(int i, int bit_length){
+        int arr_length = 0;
+        for (int j = i; j < (Short.MAX_VALUE) & (j%bit_length == 0); j++) {
+            arr_length ++;
         }
+        int[] arr_max_value = new int[arr_length];
+        for (int j = i, k = 0; j < (Short.MAX_VALUE) & j%bit_length == 0; j++) {
+            arr_max_value[k] = j;
+            k++;
+        }
+        return arr_max_value;
     }
 
-    public static void arr_min_value(int[] array_min_value,int i, int new_length){
-        System.out.println("Значения, которые все некратные n числа в диапазоне от Short.MIN_VALUE до i: ");
-        for (int j = Short.MIN_VALUE, k = 0; j < i & new_length % j != 0; j++) {
-            array_min_value[k] = j;
+    public static int[] arr_min_value(int i, int bit_length){
+        int[] arr_min_value = new int[Math.abs(Short.MIN_VALUE)];
+        for (int j = Short.MIN_VALUE, k = 0; j < i & bit_length % j != 0; j++) {
+            arr_min_value[k] = j;
             k++;
-            System.out.println(array_min_value[k]);
         }
+        return arr_min_value;
     }
 }
 
